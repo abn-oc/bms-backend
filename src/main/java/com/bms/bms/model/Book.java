aeb4c606095fbf9d40b674c103bf432d27e2fc87
abn-oc <abdullah.iht22@gmail.com>
2025-07-14 05:43:18 -0700

    securityconfiguration......m....

--- src/main/java/com/bms/bms/model/Book.java
+++ src/main/java/com/bms/bms/model/Book.java
@@ -1,9 +1,6 @@
 package com.bms.bms.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +9,7 @@ import lombok.ToString;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "Books")
 @NoArgsConstructor
 @Getter
 @Setter
---
+++ src/main/java/com/bms/bms/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.bms.bms.repository;
+
+import com.bms.bms.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
